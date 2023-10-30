4cbd47554017d7e9a8cc9eac169a66df8773e6ca
EMAinasse <47646920+EMAinasse@users.noreply.github.com>
2023-10-30 15:54:23 -0400

    Update solver.py
    @arahimyar, shout out for the implementation of `construct_design_matrix`!

--- SignedDifferences/solver.py
+++ SignedDifferences/solver.py
@@ -8,14 +8,50 @@ from dataclasses import dataclass
 
 @dataclass
 class RegSolution:
+    """A regression solution data class.
+
+    Attributes
+    ----------
+    - sol: np.ndarray
+        The regression solution, as an array.
+    - res_norm: float
+        The norm of the residual resulting from the solution.
+    """
     sol: np.ndarray
     res_norm: float
 
 class SignedDifferencesOLS:
-    def __init__(self, library = 'cvxpy'):
+    """The class for the solver of the Signed Differences OLS problem, based on a particular library.
+
+    Attribute
+    ---------
+    - library: str
+        The library to be used. (This is `cvxpy` by default as it yields the best results.)
+
+    Methods
+    -------
+    - construct_design_matrix
+        Constructs the design matrix for the equivalent vectorized problem. (See the original `Problem.md`.)
+    - solve
+        Solves the OLS problem via Non-Negative Least Squares from the selected library.
+    """
+    def __init__(self, library : str = 'cvxpy'):
         self.library = library
-    
-    def construct_design_matrix(self, n):
+
+    # Credit to @arahimyar for this implementation of the construction of the design matrix
+    def construct_design_matrix(self, n: int) -> np.ndarray:
+        """A method to construct the design matrix for the equivalent vectorized problem.
+
+        Parameter
+        ---------
+        - n: int
+            The size of the (skew-symmetric) difference matrix.
+        Returns
+        -------
+        - A: np.ndarray
+            The design matrix A for the equivalent vectorized problem.
+        """
+        
         A = np.zeros((n * (n-1) // 2, n))
         
         count = 0
@@ -29,7 +65,20 @@ class SignedDifferencesOLS:
         
         return A
     
-    def solve(self, data):
+    def solve(self, data: np.ndarray) -> RegSolution:
+        """
+        A method for solving the Signed Differences OLS problem.
+
+        Parameter
+        ---------
+        - data: np.ndarray
+            The data, given as a skew-symmetric matrix of differences. (Or approximately skew-symmetric, if noise-contaminated.)
+
+        Returns
+        -------
+        - solution: RegSolution
+            The solution as a `RegSolution` dataclass containing the solution vector, and the norm of the residual given by this solution.
+        """
         n = len(data)
         
         b = half_vectorize(data) 
@@ -63,5 +112,7 @@ class SignedDifferencesOLS:
             X_hat = reg_nnls.coef_
             
             res_norm = np.linalg.norm(A @ X_hat - b)
+
+        solution = RegSolution(sol=X_hat, res_norm=res_norm)
         
-        return RegSolution(sol=X_hat, res_norm=res_norm)
+        return solution
